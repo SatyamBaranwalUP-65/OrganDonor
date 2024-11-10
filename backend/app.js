dcddfcfd0b2135965c79ec3d5fb89f80929c6293
SatyamBaranwalUP-65 <152766676+SatyamBaranwalUP-65@users.noreply.github.com>
2024-11-10 23:36:10 +0530

    tt

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://organ-donor-brown.vercel.app/"],
     credentials: true,
   })
 );
