6a61385541ea49a9435ec6aa26bd17bfa16057fc
SatyamBaranwalUP-65 <152766676+SatyamBaranwalUP-65@users.noreply.github.com>
2024-11-10 23:44:02 +0530

    ttsds

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://organ-donor-brown.vercel.app/"],
+    origin: ["https://organ-donor-brown.vercel.app/","https://organdonor.onrender.com","http://localhost:3000"],
     credentials: true,
   })
 );
